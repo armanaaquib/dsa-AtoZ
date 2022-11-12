b0dced94f083bf2100b25888a3247979c9bc48ba
Aaquib Equbal <armanaaquibequbal@gmail.com>
2022-11-12 22:40:38 +0530

    Update step1 basics pattern1

--- src/s1_basics/patterns/pattern1.kt
+++ src/s1_basics/patterns/pattern1.kt
@@ -6,13 +6,27 @@ package s1_basics.patterns
 *****
 *****
 *****
+Question Link: https://practice.geeksforgeeks.org/problems/square-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1
  */
 
 fun main() {
-    for (i in 1..5) {
-        for (j in 1..5) {
+    val n = 5
+    for (i in 1..n) {
+        for (j in 1..n) {
             print("*")
         }
         println()
     }
 }
+
+/* Java code
+void printSquare(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+ */
