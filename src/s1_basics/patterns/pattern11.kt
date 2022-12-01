26c3d90ded21fd26d57ae0f579ee27d0432a5342
Aaquib Equbal <armanaaquibequbal@gmail.com>
2022-12-01 23:27:41 +0530

    Update step1 basics pattern 11

--- src/s1_basics/patterns/pattern11.kt
+++ src/s1_basics/patterns/pattern11.kt
@@ -6,16 +6,34 @@ package s1_basics.patterns
 1 0 1
 0 1 0 1
 1 0 1 0 1
+Problem: https://practice.geeksforgeeks.org/problems/triangle-pattern-1661718455/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_11
  */
 
 fun main() {
-    for (i in 1..5) {
-        var n = i % 2
+    val n = 5
+    for (i in 1..n) {
+        var num = i % 2
         for (j in 1..i) {
-            print(n)
+            print(num)
             print(" ")
-            n = if (n == 0) 1 else 0
+            num = if (num == 0) 1 else 0
         }
         println()
     }
 }
+
+/*
+Java code:
+    void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            int num = i % 2;
+            for (int j = 1; j <= i; j++)  {
+                System.out.print(num);
+                System.out.print(" ");
+                if (num == 0) num = 1;
+                else num = 0;
+            }
+            System.out.println();
+        }
+    }
+ */
